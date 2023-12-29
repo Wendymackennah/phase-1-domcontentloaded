a9df216aa320add01098252987b1bee0a1e2e10a
wendymackennah <wendymackennah5@gmail.com>
2023-12-29 12:12:04 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Your code goes here
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Target the paragraph element with id="text"
+    var textParagraph = document.getElementById('text');
+
+    // Check if the element is found before trying to manipulate it
+    if (textParagraph) {
+        // Replace the text content with "This is really cool!"
+        textParagraph.textContent = "This is really cool!";
+    } else {
+        console.error("Element with id='text' not found.");
+    }
+});
